9a2075e8dc250176cbfe7d46c66841e3c323b0d2
Himanxusharma <himanshugaurbsr@gmail.com>
2022-07-07 17:59:18 +0530

    commit

--- authentication/views.py
+++ authentication/views.py
@@ -4,27 +4,7 @@ from django.contrib.auth.models import User
 from django.contrib import messages
 
 def home(request):
-    if request.method=="post":
-        #Acc = requst.POST.get('Acc')
-        Acc = request.POST['Acc']
-        Amt = request.POST['Amt']
-        Pacc = request.POST['Pacc']
-        
-
-
-        #myuser = User.objects.create_user(Acc, Amt, Pacc)
-        
-        myuser.Acc = Acc
-        myuser.Amt = Amt
-        myuser.Pacc = Pacc
-
-
-        myuser.save()
-
-        messages.success(request,"Thanks, wait a while!")
-
-        return redirect('submit')
-
+    
 
 
 
@@ -32,6 +12,25 @@ def home(request):
     return render(request, "authentication/index.html")
 
 def submit(request):
+
+    if request.method == "post":
+        #Acc = requst.POST.get('Acc')
+        Acc = request.POST['Acc']
+        Amt = request.POST['Amt']
+        Pacc = request.POST['Pacc']
+
+        myuser = User.objects.create_user(Acc, Amt, Pacc)
+
+        # myuser.Acc = Acc
+        # myuser.Amt = Amt
+        # myuser.Pacc = Pacc
+
+        myuser.save()
+
+        messages.success(request, "Thanks, wait a while!")
+
+        return redirect('submit')
+
     return render(request, "authentication/submit.html")
 
 
